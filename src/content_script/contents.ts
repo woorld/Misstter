0e580b8d6ab62025ad1eb8ccbe6a54b018ba7d24
woorld <17719812+woorld@users.noreply.github.com>
2024-05-18 01:19:41 +0900

    twitter.comをx.comに修正

--- src/content_script/contents.ts
+++ src/content_script/contents.ts
@@ -1,8 +1,8 @@
-if (location.hostname === 'twitter.com') {
-  // is current location is twitter.com load twitter content script
+if (location.hostname === 'x.com') {
+  // is current location is x.com load twitter content script
   import('./Clients/twitter');
 
 } else if (location.hostname === "tweetdeck.twitter.com") {
   // is current location is tweetdeck.twitter.com load tweetdeck content script
   import('./Clients/tweetdeck');
-} 
+}
